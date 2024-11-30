b1a761a4c81c9cd5f2df08a9d1b110575d8040a2
Richard Su <suruiquan10@163.com>
2024-11-30 00:15:55 -0500

    Installed packages, changed port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const port = process.env.PORT
 const express = require('express'); 
 const app = express(); 
+require('dotenv').config();
+let port = process.env.PORT; 
 
 app.listen(port, 'localhost', () => {
     console.log('Server started on port ' + port); 
